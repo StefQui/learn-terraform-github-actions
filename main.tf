95ca5574d95da06cca6de9cd05a694803929589e
stefqui <stephane.quint@gmail.com>
2021-02-16 23:46:57 +0100

    Change orga name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 0.14"
 
   backend "remote" {
-    organization = "STEF_ORG"
+    organization = "smart-stef"
 
     workspaces {
       name = "gh-actions-demo"
