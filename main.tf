b102caa3907f578358132e016c6ff8cd26b31946
stefqui <stephane.quint@gmail.com>
2021-02-17 21:51:22 +0100

    Other image

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-020310979efa9e99d"
+  ami                    = "ami-5e0eb923"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
